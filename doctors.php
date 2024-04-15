8f9cc6bd4e7bb34ae82d7d46be93810aef614f37
GADO <halela>
2024-04-15 17:42:43 +0200

    first commit

---
+++ doctors.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/fontawesome-free-6.5.1-web/css/all.css">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.js"></script>
+    <script src="assets/fontawesome-free-6.5.1-web/js/all.min.js"></script>
+    <!-- CSS de Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- JavaScript de Bootstrap (nécessite jQuery) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+    <title>Docteurs</title>
+</head>
+<header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="#"> Dokotoro</a>
+            </div>
+
+           
+            <ul class="menu">
+                <li> <i class="fas fa-briefcase-medical" style="color: #ffFFFF;"></i> <a href="#">S'informer</a></li>
+                <li> <i class="fas fa-stethoscope" style="color: #ffFFFF;"></i> <a href="#">Se former </a></li>
+                <li> <i class="fas fa-hospital" style="color: #ffFFFF;"></i> <a href="#">se soigner </a></li>
+            </ul>
+       
+            <div class="icons">
+                <a href="#"><i class="fas fa-shopping-cart"></i></a>
+                <a href="#"><i class="fas fa-search"></i></a>
+                <a href="#"><i class="fas fa-user"></i>
+                </a>
+            </div> 
+        </nav>
+    </header>
+    <div class="explore">
+    <h2> Annuaire des professionnels de Santé </h2>
+  </div>
+  <div class="container mt-5">
+    <div class="card">
+        <img src="photo_docteur.jpg" class="card-img-top" alt="Photo du docteur">
+        <div class="card-body">
+            <h5 class="card-title">Dr. John Doe</h5>
+            <p class="card-text">Spécialité: Médecine générale</p>
+            <p class="card-text">Horaires de consultation: Lundi - Vendredi (9h00 - 17h00)</p>
+            <p class="card-text">Email: johndoe@example.com</p>
+            <p class="card-text">Téléphone: +1234567890</p>
+            <a href="#" class="btn btn-primary ">Prendre rendez-vous</a>
+        </div>
+    </div>
+</div>
+
+<body>
+    
+</body>
+</html>
